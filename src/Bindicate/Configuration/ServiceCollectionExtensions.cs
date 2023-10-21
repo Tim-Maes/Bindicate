d7757aa1144222eee10c242e052b1b1077cefeb2
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2023-10-21 20:48:50 +0200

    Cleanup usings

--- src/Bindicate/Configuration/ServiceCollectionExtensions.cs
+++ src/Bindicate/Configuration/ServiceCollectionExtensions.cs
@@ -1,20 +1,10 @@
-﻿using Bindicate.Attributes;
-using Bindicate.Attributes.Options;
-using Bindicate.Lifetime;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
-using System.Diagnostics.Metrics;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using System.Runtime.Intrinsics.X86;
 
 namespace Bindicate.Configuration;
 
 public static class ServiceCollectionExtensions
 {
     public static AutowiringBuilder AddAutowiringForAssembly(this IServiceCollection services, Assembly targetAssembly)
-    {
-        return new AutowiringBuilder(services, targetAssembly);
-    }
+        => new AutowiringBuilder(services, targetAssembly);
 }
